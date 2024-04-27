d061f614c769a34c982c3ce286ba114885f4e637
Saad Ahmed <saadahmedrajput99@gmail.com>
2024-04-28 00:14:45 +0200

    Feat: Complete IOS Calculator.

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Informamuse on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
